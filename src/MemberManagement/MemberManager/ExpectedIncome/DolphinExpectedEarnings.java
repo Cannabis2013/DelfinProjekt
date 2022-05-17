2505355e1beb7d9e970910e85ae124ab20576aca
Cannabis2013 <MH2012@outlook.dk>
2022-05-17 12:52:43 +0200

    Simplified a little

--- src/MemberManagement/MemberManager/ExpectedIncome/DolphinExpectedEarnings.java
+++ src/MemberManagement/MemberManager/ExpectedIncome/DolphinExpectedEarnings.java
@@ -2,18 +2,9 @@ package MemberManagement.MemberManager.ExpectedIncome;
 
 import MemberManagement.MemberManager.CreateMembers.Member;
 import MemberManagement.MemberManager.CreateMembers.SubscriptionStatus;
-
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
-
-/*
-For aktive medlemmer er kontingentet for ungdomssvømmere (under 18 år) 1000 kr. årligt,
-for seniorsvømmere (18 år og over) 1600 kr. årligt.
-For medlemmer over 60 år gives der 25 % rabat af seniortaksten.
-For passivt medlemskab er taksten 500 kr. årligt.
- */
 
 public class DolphinExpectedEarnings implements ExpectedEarnings{
     private final int DISCOUNT_THRESHOLD_AGE = 60;
@@ -22,34 +13,38 @@ public class DolphinExpectedEarnings implements ExpectedEarnings{
     private final int JUNIOR_RATE = 1000;
     private final int SENIOR_RATE = 1600;
     private final double SENIOR_DISCOUNT = 0.75; // 1 - 0.25 = 0.75
-    private int getAge(LocalDate birthDate){
+
+    private int getAgeByBirthDate(LocalDate birthDate){
         var now = LocalDate.now();
         var period = Period.between(birthDate,now);
         var years = period.getYears();
         return years;
     }
+
+    private int calculateActiveRate(Member member){
+        var age = getAgeByBirthDate(member.birthDate());
+        if(age > DISCOUNT_THRESHOLD_AGE){
+            var amount = SENIOR_RATE*SENIOR_DISCOUNT;
+            return (int) amount;
+        }
+        else if(age >= SENIOR_THRESHOLD_AGE){
+            return SENIOR_RATE;
+        }
+        else{
+            return JUNIOR_RATE;
+        }
+    }
+
     @Override
     public int expected(List<Member> members) {
-        var result = 0;
-        for (var i = 0; i<members.size();i++){
-            var member = members.get(i);
-            var age = getAge(member.birthDate());
-            var status = member.status();
+        final int[] result = {0};
+        members.forEach(m -> {
+            var status = m.status();
             if(status == SubscriptionStatus.PASSIVE)
-                result += PASSIVE_RATE;
-            else{
-                if(age > DISCOUNT_THRESHOLD_AGE){
-                    var amount = SENIOR_RATE*SENIOR_DISCOUNT;
-                    result += amount;
-                }
-                else if(age >= SENIOR_THRESHOLD_AGE){
-                    result += SENIOR_RATE;
-                }
-                else{
-                    result += JUNIOR_RATE;
-                }
-            }
-        }
-        return result;
+                result[0] += PASSIVE_RATE;
+            else
+                result[0] += calculateActiveRate(m);
+        });
+        return result[0];
     }
 }
