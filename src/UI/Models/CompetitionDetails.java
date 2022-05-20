6751fd4f188d0a8037d3c6254fc378de86c5e9b4
stkea <stef240p@stud.kea.dk>
2022-05-20 12:51:40 +0200

    UI done for competition result registration

---
+++ src/UI/Models/CompetitionDetails.java
@@ -0,0 +1,5 @@
+package UI.Models;
+
+
+public record CompetitionDetails(String id, String convention, String date, int rank, String result) {
+}
--- src/UI/PrintScreen/RegisterCompetitionResultScreen.java
+++ src/UI/PrintScreen/RegisterCompetitionResultScreen.java
@@ -1,11 +1,24 @@
 package UI.PrintScreen;
 
 import Backend.Contracts.BackendDomain;
+import UI.Contracts.PrintScreen;
 import UI.Contracts.PrintScreenByDomain;
+import UI.Contracts.ReadUserInput;
+import UI.ReadUserInput.ConsoleHaltForInput;
 
-public class RegisterCompetitionResultScreen implements PrintScreenByDomain {
+public class RegisterCompetitionResultScreen implements PrintScreen {
+
+    private PrintScreen _blankScreen = new PrintBlankScreen();
+    private ReadUserInput<String> _halt = new ConsoleHaltForInput();
     @Override
-    public void print(BackendDomain domain) {
-
+    public void print() {
+        _blankScreen.print();
+        System.out.println("""
+                ------------------------
+                Competition result added
+                ------------------------
+                """);
+        _halt.read();
+        _blankScreen.print();
     }
 }
